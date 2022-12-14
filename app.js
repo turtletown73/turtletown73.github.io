cb82b4b36ff9f035d51aa18e73d3c7bffaa11220
turtletown73 <108070070+turtletown73@users.noreply.github.com>
2022-12-14 14:09:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+canvas.requestPointerLock();
+
 const dog = document.getElementById('dogimage');
 
 document.addEventListener(function(event) {
   if (event.keyCode == 87) {
-    alert('w was pressed')
+    alert('w was pressed');
   }
 })
