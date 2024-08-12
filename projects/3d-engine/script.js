0520cc2ff9d2dac6cf729e8a2d4bc0df1faab096
Scoobis Trombone <108070070+turtletown73@users.noreply.github.com>
2024-08-12 14:59:43 +0000

    yay

--- projects/3d-engine/script.js
+++ projects/3d-engine/script.js
@@ -3,14 +3,14 @@ let ctx = canvas.getContext("2d");
 ctx.strokeStyle = "white";
 
 let vertices = [
-    {x:0,y:0,z:1},
-    {x:1,y:0,z:1},
-    {x:0,y:1,z:1},
-    {x:1,y:1,z:1},
-    {x:0,y:1,z:0},
-    {x:1,y:1,z:0},
-    {x:0,y:0,z:0},
-    {x:1,y:0,z:0},
+    {x:0,y:0,z:6},
+    {x:1,y:0,z:6},
+    {x:0,y:1,z:6},
+    {x:1,y:1,z:6},
+    {x:0,y:1,z:5},
+    {x:1,y:1,z:5},
+    {x:0,y:0,z:5},
+    {x:1,y:0,z:5},
 ];
 
 let triangles = [
