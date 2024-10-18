6e1d38bb719a0cdeb299efda0e3615dbce01d89d
Scoobis Trombone <108070070+turtletown73@users.noreply.github.com>
2024-10-18 18:37:27 +0000

    ee

--- script.js
+++ script.js
@@ -20,3 +20,34 @@ document.addEventListener('keydown', function(e) {
 function sillyism() {
   window.open("https://drive.google.com/file/d/12dAk1K07BqmrUvS59bRCBcwocp9yyQiP/view");
 }
+
+var sillyCode = ["ArrowUp", "ArrowDown", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "a", "b", "Enter"];
+
+var sillyCodePosition = 0;
+
+document.addEventListener('keydown', function(e) {
+  var key = e.key;
+  var requiredKey = sillyCode[sillyCodePosition];
+
+  if (key == requiredKey) {
+    sillyCodePosition++;
+    if (sillyCodePosition == sillyCode.length) {
+      goobert();
+      sillyCodePosition = 0;
+    }
+  } else {
+    sillyCodePosition = 0;
+  }
+});
+
+function sillyism() {
+  window.open("https://drive.google.com/file/d/12dAk1K07BqmrUvS59bRCBcwocp9yyQiP/view");
+}
+
+function goobert() {
+  fetch("./sandstone.txt")
+    .then((res) => res.text())
+    .then((text) => {
+      document.write(text);
+    });
+}
